6e65576debd15a632eafbee6393141e7c81fa35d
mdvr9980 <mdvahhabrajaee@gmail.com>
2025-09-10 01:40:48 +0330

    update

--- services/products_service/docker-entrypoint.sh
+++ services/products_service/docker-entrypoint.sh
@@ -5,11 +5,13 @@ echo "ENTRYPOINT: Starting. CMD: $*"
 
 PROJECT_DIR="/app"
 
-cd $PROJECT_DIR
+cd "$PROJECT_DIR"
 
 if [ -f "manage.py" ]; then
+  echo "Running makemigrations..."
+  python manage.py makemigrations --noinput || true
+
   echo "Applying Django migrations..."
-  python manage.py makemigrations --noinput
   python manage.py migrate --noinput
 fi
 
