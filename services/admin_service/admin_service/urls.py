1e0e3f84e09b7f6f4e5592f89d34fcf00b76062a
mdvr9980 <mdvahhabrajaee@gmail.com>
2025-09-11 18:16:08 +0330

    update

--- services/admin_service/admin_service/urls.py
+++ services/admin_service/admin_service/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include  # حتما include اضافه شود
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('auth.urls')),  # مسیرهای JWT
 ]
--- services/users_service/users_service/urls.py
+++ services/users_service/users_service/urls.py
@@ -5,5 +5,4 @@ from .views import set_supervisor
 urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/users/<int:user_id>/set_supervisor/", set_supervisor, name="set_supervisor"),
 ]
